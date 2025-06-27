4dd104c986a49d702f31d3c8a65bd15a438a3e11
wltschmrz <gun0lee20@gmail.com>
2025-06-27 16:21:47 +0000

    Changes to be committed: 	modified:   utils/data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -41,6 +41,7 @@ def load_data(data_path='./data', device=torch.device('cpu')):
 
     return kg_g.to(device), smiles_list
 
+
 def update_food_data_with_indices(data_path='./data'):
     # 경로 설정
     smiles_file = os.path.join(data_path, 'smiles.tsv')
@@ -107,6 +108,7 @@ def update_food_data_with_indices(data_path='./data'):
 
     return food_node_indexed_file, food_edge_indexed_file, smiles_file
 
+
 def get_train_test(data_path='./data', fold_num=5, label_type='multi_class', condition='s1'):
     sample = pd.read_csv(os.path.join(data_path, 'ddi.tsv'), sep='\t').values
     kfold = KFold(n_splits=fold_num, shuffle=True, random_state=42, up_sample=(label_type == 'multi_class'), condition=condition)
@@ -117,68 +119,70 @@ def get_train_test(data_path='./data', fold_num=5, label_type='multi_class', con
         test_sample.append(test)
 
     # generate negative samples
-    if label_type == 'binary_class' or label_type == 'multi_label':
-        if label_type == 'multi_label':
-            all_ddi = pd.read_csv(os.path.join(data_path, 'ddi.tsv'), sep='\t').values
+    if label_type not in ['binary_class', 'multi_label']:
+        return train_sample, test_sample
+
+    if label_type == 'multi_label':
+        all_ddi = pd.read_csv(os.path.join(data_path, 'ddi.tsv'), sep='\t').values
+    else:
+        all_ddi = pd.read_csv(os.path.join(data_path, 'ddi.tsv'), sep='\t').values[:, :2]
+    ddi = set()
+    for item in all_ddi:
+        ddi.add('\t'.join([str(_) for _ in item]))
+
+    for fold in range(fold_num):
+        if condition == 's1':
+            train_drug = np.arange(max(train_sample[fold][:, :2].max(), test_sample[fold][:, :2].max())+1)
+            test_drug = train_drug
         else:
-            all_ddi = pd.read_csv(os.path.join(data_path, 'ddi.tsv'), sep='\t').values[:, :2]
-        ddi = set()
-        for item in all_ddi:
-            ddi.add('\t'.join([str(_) for _ in item]))
+            train_drug = np.unique(train_sample[fold][:, :2])
+            test_drug = np.unique(test_sample[fold][:, :2])
 
-        for fold in range(fold_num):
-            if condition == 's1':
-                train_drug = np.arange(max(train_sample[fold][:, :2].max(), test_sample[fold][:, :2].max())+1)
-                test_drug = train_drug
-            else:
-                train_drug = np.unique(train_sample[fold][:, :2])
-                test_drug = np.unique(test_sample[fold][:, :2])
+        if label_type == 'binary_class':
+            train_sample[fold][:, 2] = 1
+            test_sample[fold][:, 2] = 1
 
-            if label_type == 'binary_class':
-                train_sample[fold][:, 2] = 1
-                test_sample[fold][:, 2] = 1
+        sample_neg = []
+        sample_pos = np.concatenate([train_sample[fold], test_sample[fold]])
+        for i in range(len(train_sample[fold])):
+            while True:
+                d1 = train_drug[np.random.randint(len(train_drug))]
+                d2 = train_drug[np.random.randint(len(train_drug))]
+                if d1 == d2:
+                    continue
+                if label_type == 'binary_class' and ('\t'.join([str(d1), str(d2)]) not in ddi and '\t'.join([str(d2), str(d1)]) not in ddi):
+                    sample_neg.append([d1, d2, 0])
+                    break
+                elif label_type == 'multi_label' and ('\t'.join([str(d1), str(d2), str(sample_pos[i][2])]) not in ddi and '\t'.join([str(d2), str(d1), str(sample_pos[i][2])]) not in ddi):
+                    sample_neg.append([d1, d2, 0])
+                    break
+        for i in range(len(train_sample[fold]), len(train_sample[fold])+len(test_sample[fold])):
+            while True:
+                d1 = test_drug[np.random.randint(len(test_drug))]
+                d2 = test_drug[np.random.randint(len(test_drug))]
+                if d1 == d2:
+                    continue
+                if label_type == 'binary_class' and ('\t'.join([str(d1), str(d2)]) not in ddi and '\t'.join([str(d2), str(d1)]) not in ddi):
+                    sample_neg.append([d1, d2, 0])
+                    break
+                elif label_type == 'multi_label' and ('\t'.join([str(d1), str(d2), str(sample_pos[i][2])]) not in ddi and '\t'.join([str(d2), str(d1), str(sample_pos[i][2])]) not in ddi):
+                    sample_neg.append([d1, d2, 0])
+                    break
+        train_sample_neg = np.array(sample_neg[:len(train_sample[fold])])
+        test_sample_neg = np.array(sample_neg[len(train_sample[fold]):])
+        if label_type == 'multi_label':
+            train_sample[fold] = np.concatenate([train_sample[fold], train_sample[fold][:, 2:]], axis=-1)
+            test_sample[fold] = np.concatenate([test_sample[fold], test_sample[fold][:, 2:]], axis=-1)
+            train_sample[fold][:, 2] = 1
+            test_sample[fold][:, 2] = 1
 
-            sample_neg = []
-            sample_pos = np.concatenate([train_sample[fold], test_sample[fold]])
-            for i in range(len(train_sample[fold])):
-                while True:
-                    d1 = train_drug[np.random.randint(len(train_drug))]
-                    d2 = train_drug[np.random.randint(len(train_drug))]
-                    if d1 == d2:
-                        continue
-                    if label_type == 'binary_class' and ('\t'.join([str(d1), str(d2)]) not in ddi and '\t'.join([str(d2), str(d1)]) not in ddi):
-                        sample_neg.append([d1, d2, 0])
-                        break
-                    elif label_type == 'multi_label' and ('\t'.join([str(d1), str(d2), str(sample_pos[i][2])]) not in ddi and '\t'.join([str(d2), str(d1), str(sample_pos[i][2])]) not in ddi):
-                        sample_neg.append([d1, d2, 0])
-                        break
-            for i in range(len(train_sample[fold]), len(train_sample[fold])+len(test_sample[fold])):
-                while True:
-                    d1 = test_drug[np.random.randint(len(test_drug))]
-                    d2 = test_drug[np.random.randint(len(test_drug))]
-                    if d1 == d2:
-                        continue
-                    if label_type == 'binary_class' and ('\t'.join([str(d1), str(d2)]) not in ddi and '\t'.join([str(d2), str(d1)]) not in ddi):
-                        sample_neg.append([d1, d2, 0])
-                        break
-                    elif label_type == 'multi_label' and ('\t'.join([str(d1), str(d2), str(sample_pos[i][2])]) not in ddi and '\t'.join([str(d2), str(d1), str(sample_pos[i][2])]) not in ddi):
-                        sample_neg.append([d1, d2, 0])
-                        break
-            train_sample_neg = np.array(sample_neg[:len(train_sample[fold])])
-            test_sample_neg = np.array(sample_neg[len(train_sample[fold]):])
-            if label_type == 'multi_label':
-                train_sample[fold] = np.concatenate([train_sample[fold], train_sample[fold][:, 2:]], axis=-1)
-                test_sample[fold] = np.concatenate([test_sample[fold], test_sample[fold][:, 2:]], axis=-1)
-                train_sample[fold][:, 2] = 1
-                test_sample[fold][:, 2] = 1
+            train_sample_neg = np.concatenate([train_sample_neg, np.zeros((train_sample_neg.shape[0], 1), dtype=train_sample_neg.dtype)], axis=-1)
+            test_sample_neg = np.concatenate([test_sample_neg, np.zeros((test_sample_neg.shape[0], 1), dtype=test_sample_neg.dtype)], axis=-1)
+            train_sample_neg[:, 3] = train_sample[fold][:, 3]
+            test_sample_neg[:, 3] = test_sample[fold][:, 3]
 
-                train_sample_neg = np.concatenate([train_sample_neg, np.zeros((train_sample_neg.shape[0], 1), dtype=train_sample_neg.dtype)], axis=-1)
-                test_sample_neg = np.concatenate([test_sample_neg, np.zeros((test_sample_neg.shape[0], 1), dtype=test_sample_neg.dtype)], axis=-1)
-                train_sample_neg[:, 3] = train_sample[fold][:, 3]
-                test_sample_neg[:, 3] = test_sample[fold][:, 3]
-
-            train_sample[fold] = np.concatenate([train_sample[fold], train_sample_neg])
-            test_sample[fold] = np.concatenate([test_sample[fold], test_sample_neg])
+        train_sample[fold] = np.concatenate([train_sample[fold], train_sample_neg])
+        test_sample[fold] = np.concatenate([test_sample[fold], test_sample_neg])
 
     return train_sample, test_sample
 
